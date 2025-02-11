b6610b2c1a9933aaf04fe9cb1b05fbf5679e0a0f
Augis <augustas.stankevicius@tuta.io>
2025-02-11 20:32:13 +0200

    SCRUM-1 test

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -21,11 +21,5 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
-	#var directionY := Input.get_axis("ui_up", "ui_down")
-	#if directionY:
-		#velocity.y = directionY * SPEED
-	#else:
-		#velocity.y = move_toward(velocity.y, 0, SPEED)
 
 	move_and_slide()
