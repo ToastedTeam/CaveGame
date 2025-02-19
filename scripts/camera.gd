9ea6f63055fd859e64092564ccae62eeb644fb04
ajud123 <audriusbertasius1@gmail.com>
2025-02-19 16:27:27 +0200

    messed around the subpixel movement, failed to do anyhting meaningful

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -2,6 +2,7 @@ class_name GameplayCamera
 extends Camera2D
 
 @export var followTarget: Node2D;
+#var lastPosition: Vector2 = Vector2.ZERO;
 
 var viewport_shader: ShaderMaterial
 
@@ -10,15 +11,17 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void: 
+	# POTENTIAL FIX: Try applying the sub-pixel adjustment in physics process?
+	# personally didn't see a change but maybe it can help.
+	
 	#if position.distance_to(followTarget.position) > 0.5:
 		#position = position.lerp(followTarget.position, 10 * delta)
 	#else:
 		#position = followTarget.position
 	#print(7 * delta)
 	
-	position = position.lerp(followTarget.position, 7 * delta)
+	#position = position.lerp(followTarget.position, 7 * delta)
 	var sub_pixel : Vector2 = Vector2(
 		global_position.round().x - global_position.x,
 		global_position.round().y - global_position.y
@@ -27,12 +30,15 @@ func _process(delta: float) -> void:
 	if sub_pixel.length() < 0.1:
 		sub_pixel = Vector2.ZERO
 		
+	#print(followTarget.position.distance_to(lastPosition))
 		
+	#lastPosition = followTarget.position;
 	if viewport_shader:
 		viewport_shader.set_shader_parameter("cam_offset", sub_pixel)
 		viewport_shader.set_shader_parameter("cam_position", global_position)
 		
 		pass
+	
 	#pass
 	
 	
