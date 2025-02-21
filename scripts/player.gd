6771623daab11b9f505b15de694d99fa19917d6d
Augis <augustas.stankevicius@tuta.io>
2025-02-21 12:00:30 +0200

    SCRUM-1 basic movement animations

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,9 @@
 extends CharacterBody2D
 
-
 const SPEED = 120.0
 const JUMP_VELOCITY = -300.0
 
+@onready var player_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -22,5 +22,20 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+		
+#	Player animations
+	if is_on_floor():
+		if direction:
+			player_sprite.play("run")
+		else:
+			player_sprite.play("idle")
+	else:
+		player_sprite.play("jump")
+	
+	if direction < 0:
+		player_sprite.flip_h = true
+	elif direction > 0:
+		player_sprite.flip_h = false
 
 	move_and_slide()
