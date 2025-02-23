1a851baf954e68e2b916a6538de8b96f8eeb5a1a
Pamda <105165615+Panda1941@users.noreply.github.com>
2025-02-23 19:16:01 +0200

    Added base player stats + Some UI elements
    UI doesn't work and I don't know why.
    Added a few sprites for the UI.

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,25 @@
 extends CharacterBody2D
 
+# Defining Player constants
 const SPEED = 120.0
 const JUMP_VELOCITY = -300.0
+const MAX_HP = 20
+const MAX_MANA = 100
+const BASE_DAMAGE = 5
 
 @onready var player_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+# Defining these as variables because they can change overtime
+var current_hp
+var current_mana
+var damage
+
+# Setting current 
+func _ready() -> void:
+	current_hp = MAX_HP
+	current_mana = MAX_MANA
+	damage = BASE_DAMAGE
+	print("player hp: ", current_hp, " player mana: ", current_mana, " player damage: ", damage)
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -13,6 +29,10 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("player_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
+	if Input.is_action_just_pressed("DEBUG_takeDamage"):
+		changeHealth(-5)
+		print("Current hp: ", current_hp)
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -38,3 +58,13 @@ func _physics_process(delta: float) -> void:
 		player_sprite.flip_h = true
 	elif direction > 0:
 		player_sprite.flip_h = false
+
+
+func changeHealth(amount):
+	current_hp += amount
+	if current_hp <= 0:
+		current_hp = 0
+		print("The player has Died")
+	
+func changeMana(amount):
+	current_mana += amount
