d67d2f41709306ce2dc2b97af8a3448216ba7677
Pamda <105165615+Panda1941@users.noreply.github.com>
2025-02-24 20:09:10 +0200

    Fixed the things mentioned in the convo
    - HP Bar renamed to HealthBar
    - Mana Bar renamed to ManaBar
    
    - health_bar and mana_bar variables are now "dynamic" with the export thingy
    
    - current_hp and current_mana now have a getter and a setter, these just set the variables and update the UI
    
    - added the bar min_value variables to the _ready() function
    
    - moved the if current_hp < 0 checks to the debug input thing

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,14 +7,50 @@ const MAX_HP = 20
 const MAX_MANA = 100
 const BASE_DAMAGE = 5
 
+# Various Properties
 @onready var player_sprite: AnimatedSprite2D = $AnimatedSprite2D
-@onready var health_bar: TextureProgressBar = $"../Interface/Interface2/VBoxContainer/HP Bar/HealthBar"
-@onready var mana_bar: TextureProgressBar = $"../Interface/Interface2/VBoxContainer/Mana Bar/ManaBar"
+@export var health_bar: TextureProgressBar
+@export var mana_bar: TextureProgressBar
 
-# Defining these as variables because they can change overtime
-var current_hp
-var current_mana
-var damage
+# Variables
+var current_hp: int:
+	get:
+		return current_hp
+	set(value):
+		# These seem to not work as I want
+		# If current_hp <= 0 then it always sets it to 0 (ig cuz this is before _ready() script)
+		# Otherwise it allows HP to go into the negatives. Commented this out for now, maybe
+		# this can be fixed or implemented somehow different.
+		# Same thing with current_mana variable
+		# Currently moved to _physics_process() function
+		
+		#if current_hp < 0:
+			#print("The player has died")
+			#current_hp = 0
+		#else:
+			#current_hp = value
+			
+		current_hp = value
+		health_bar.value = current_hp
+
+var current_mana: int:
+	get:
+		return current_mana
+	set(value):
+		#if current_mana < 0:
+			#print("The player has run out of Mana")
+			#current_mana = 0
+		#else:
+			#current_mana = value
+		
+		current_mana = value
+		mana_bar.value = current_mana
+
+var damage: int:
+	get:
+		return damage
+	set(value):
+		damage = value
 
 # Setting current 
 func _ready() -> void:
@@ -24,6 +60,8 @@ func _ready() -> void:
 	
 	health_bar.max_value = MAX_HP
 	mana_bar.max_value = MAX_MANA
+	health_bar.min_value = 0	# This is also set to 0 in the ProgressBar Node
+	mana_bar.min_value = 0		# This is also set to 0 in the ProgressBar Node
 	
 	health_bar.value = current_hp
 	mana_bar.value = current_mana
@@ -33,9 +71,17 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	# DEBUG STUFF SO ITS EASY TO ACCESS
 	if Input.is_action_just_pressed("DEBUG_takeDamage"):
-		changeHealth(-5)
-		changeMana(-5)
-		print("Current hp: ", current_hp)
+		current_hp -= 5
+		current_mana -= 5
+		
+		if current_hp <= 0:
+			current_hp = 0
+			print("The player has died")
+		if current_mana <= 0:
+			current_mana = 0
+			print("The player has ran out of mana")
+	
+		print("Current hp: ", current_hp, "; Current mana: ", current_mana)
 	
 	
 	
@@ -71,17 +117,3 @@ func _physics_process(delta: float) -> void:
 		player_sprite.flip_h = true
 	elif direction > 0:
 		player_sprite.flip_h = false
-
-
-func changeHealth(amount):
-	current_hp += amount
-	health_bar.value = current_hp
-	if current_hp <= 0:
-		current_hp = 0
-		health_bar.value = current_hp
-		print("The player has Died")
-	
-func changeMana(amount):
-	if current_mana > 0:
-		current_mana += amount
-		mana_bar.value = current_mana
