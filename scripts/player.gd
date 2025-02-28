636728c10f506193087f3ad2ef5766f7137a5adc
Pamda <105165615+Panda1941@users.noreply.github.com>
2025-02-28 23:41:11 +0200

    added basic dash functionality. Cleaned up some code
    Removed older comments and some commented out lines of code (nothing significant)
    
    Added the ability for the player to dash by pressing 'd'. The dash has a 5 second cooldown, dashes a short distance.
    
    Technical notes are followed.
    Acceptance criteria are met.
    
    The player is allowed to dash mid-air to allow for more skill expressive combos i.e. dashing through some cut out in some wall or whatever else.

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,9 +6,15 @@ const JUMP_VELOCITY = -300.0
 const MAX_HP = 20
 const MAX_MANA = 100
 const BASE_DAMAGE = 5
+const DASH_SPEED = SPEED * 6
+const DASH_LENGTH = 0.15
+
 
 # Various Properties
 @onready var player_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var dash = $Dash
+@onready var dashCooldownBar = $DashCooldownBar
+@onready var dashParticles = $DashParticles
 @export var health_bar: TextureProgressBar
 @export var mana_bar: TextureProgressBar
 
@@ -17,18 +23,6 @@ var current_hp: int = MAX_HP:
 	get:
 		return current_hp
 	set(value):
-		# These seem to not work as I want
-		# If current_hp <= 0 then it always sets it to 0 (ig cuz this is before _ready() script)
-		# Otherwise it allows HP to go into the negatives. Commented this out for now, maybe
-		# this can be fixed or implemented somehow different.
-		# Same thing with current_mana variable
-		# Currently moved to _physics_process() function
-		
-		# Fixed, you were checking the current hp, which is not the same as value, aka the new
-		# value, which is called "value". That allowed it to reach negative values. However, 
-		# below the if statement there was a `current_hp = value` statement which simply negates
-		# the if statement
-		# - Toaster
 		if value < 0:
 			print("The player has died")
 			current_hp = 0
@@ -56,15 +50,11 @@ var damage: int = BASE_DAMAGE:
 		damage = value
 
 # Setting current 
-func _ready() -> void:
-	#current_hp = MAX_HP
-	#current_mana = MAX_MANA
-	#damage = BASE_DAMAGE
-	
+func _ready() -> void:	
 	health_bar.max_value = MAX_HP
 	mana_bar.max_value = MAX_MANA
-	health_bar.min_value = 0	# This is also set to 0 in the ProgressBar Node
-	mana_bar.min_value = 0		# This is also set to 0 in the ProgressBar Node
+	dashCooldownBar.max_value = dash.DASH_CD
+	# Min value setters removed, unnecessary, are set via the nodes
 	
 	health_bar.value = current_hp
 	mana_bar.value = current_mana
@@ -77,6 +67,22 @@ func _physics_process(delta: float) -> void:
 		current_hp -= 5
 		current_mana -= 5
 		print("Current hp: ", current_hp, "; Current mana: ", current_mana)
+
+	# Dashing
+	# Extra checks to prevent dashing while standing still
+	# basically you have to click 'd' and have one of the directions pressed
+	var isMoving := Input.get_axis("player_move_left", "player_move_right")
+	if Input.is_action_just_pressed("player_dash") and dash.isReady() and isMoving:
+		dash.startDash(DASH_LENGTH)
+		dashParticles.restart()
+		
+	var speed = DASH_SPEED if dash.isDashing() else SPEED
+	
+	if dash.getCooldown() > 0:
+		dashCooldownBar.show()
+		dashCooldownBar.value = dash.DASH_CD - dash.getCooldown()
+	else:
+		dashCooldownBar.hide()
 
 	# Add the gravity.
 	if not is_on_floor():
@@ -91,9 +97,9 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("player_move_left", "player_move_right")
 	
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
 		
 	move_and_slide()
 		
