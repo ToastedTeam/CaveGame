f51d0d7fddd40149a8dc3357b63709400c965e6c
ajud123 <audriusbertasius1@gmail.com>
2025-03-07 19:04:00 +0200

    Add invincibility frames to prevent melee attack spamming. The invincibility frames is added to the base entity class and any entity can have the maximum amount of invincibility frames adjusted per entity basis.

--- scripts/target_dummy.gd
+++ scripts/target_dummy.gd
@@ -1,6 +1,8 @@
 extends Entity
 
 func Damage(damage: float, source: Node2D):
+	if invincibilityFrames > 0:
+		return;
 	#Log.info("dummy got hit by " + source.name + " for " + str(damage) + " damage, health left: " + str(health))
 	var force = (($body.global_position - source.global_position) as Vector2).normalized() * damage * 50
 	$body.apply_impulse(force)
@@ -12,12 +14,9 @@ func Damage(damage: float, source: Node2D):
 	tween.tween_property(self, "modulate", Color.RED, 0.05);
 	tween.tween_property(self, "modulate", Color.WHITE, 0.05);
 	$Hurt.play()
+	invincibilityFrames = maxInvincibilityFrames;
 	pass
 
 func _timer_end() -> void:
 	$Damage.text = ""
-	pass
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
 	pass
