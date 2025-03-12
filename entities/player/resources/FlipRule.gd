c583705ac9e769cfc790cd93a4b20b3e4a6e94f3
ajud123 <audriusbertasius1@gmail.com>
2025-03-12 12:01:40 +0200

    Make limbs hide behind each other when turned. Fix head becoming slightly shorter when turned. Hopefully made the rear arm more visible?

---
+++ entities/player/resources/FlipRule.gd
@@ -0,0 +1,9 @@
+class_name FlipRule
+extends Resource
+
+@export var NodeToFlip: String;
+@export var OriginalPosition: int;
+@export var FlippedPosition: int;
+@export var changeModulation: bool = false;
+@export var normalModulation: Color;
+@export var flippedModulation: Color;
