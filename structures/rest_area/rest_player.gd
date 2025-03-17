0d8e5ead3493a4bfde29fd8bd942cde7630f9482
ajud123 <audriusbertasius1@gmail.com>
2025-03-17 20:27:13 +0200

    Add weapon changing

--- structures/rest_area/rest_player.gd
+++ structures/rest_area/rest_player.gd
@@ -4,9 +4,23 @@ func makePlayerRest(player: PlayerCharacter):
 	if !resting:
 		player.animPlayer.play("restFloor")
 		resting = true
+		var UI = get_tree().get_nodes_in_group("UI")[0]
+		var weapons: Control = UI.get_node("WeaponSelector")
+		weapons.show()
 		return
 	else:
 		player.animPlayer.play("RESET")
 		resting = false
+		var UI = get_tree().get_nodes_in_group("UI")[0]
+		var weapons: Control = UI.get_node("WeaponSelector")
+		var attacks: AttackSelectionController = UI.get_node("WeaponSelector/Margin/Attacks")
+		var fliphandler = player.get_node("sprites/FlipHandler")
+		for child in fliphandler.get_children():
+			child.queue_free()
+			pass
+		var inst = attacks.Table.GetData(attacks.Table.MeleeWeapons[attacks.currentMeleeWeapon].name).asset.instantiate()
+		fliphandler.add_child(inst)
+		
+		weapons.hide()
 		return
 	pass
