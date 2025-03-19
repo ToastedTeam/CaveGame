7cd9ccf6fa85b792f71d3258fda1468542e75c49
Grotthus <150602830+Grotthus@users.noreply.github.com>
2025-03-19 15:42:09 +0200

    Scrum 20 main menu screen
    Implemented a main menus screen with 3 buttons. Everything seems to be working fine

--- scenes/MenuScenes/main_menu.gd
+++ scenes/MenuScenes/main_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 
-func _on_p_lay_pressed() -> void:
+func _on_play_pressed() -> void:
 	get_tree().paused=false;
 	get_tree().change_scene_to_file("res://scenes/main/main.tscn")
 
