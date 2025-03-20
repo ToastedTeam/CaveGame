bab2d16a2c8b2427d730c4fbd2c69fe5cc4f9ce6
ajud123 <audriusbertasius1@gmail.com>
2025-03-20 09:51:44 +0200

    Retain resolution within the settings screen after closing it.

--- main_menu.gd
+++ main_menu.gd
@@ -10,7 +10,7 @@ func _on_options_pressed() -> void:
 	if not get_node_or_null("settings"):  # Prevent multiple instances
 		var settings_instance = settings_scene.instantiate()
 		add_child(settings_instance)  # Adds it as a child
-
+	
 
 
 func _on_exit_pressed() -> void:
