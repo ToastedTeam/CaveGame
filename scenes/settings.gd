83d85f8a3d8f79b0019dfd614e99e6ac8a171f60
Grotthus <150602830+Grotthus@users.noreply.github.com>
2025-04-01 22:35:18 +0300

    Scrum 22 settings screen completion
    Completed the settings screen with key bindings and a reset button that resets the given key bindings

--- scenes/settings.gd
+++ scenes/settings.gd
@@ -2,6 +2,19 @@ extends Control
 
 const resolutions = [ Vector2i(1920,1080), Vector2i(1600,900), Vector2i(1280,720) ]
 
+@onready var input_button_scene = preload("res://scenes/input_button.tscn")
+@onready var action_list = $PanelContainer/VBoxContainer/ScrollContainer/ActionList
+
+var is_remaping = false
+var action_to_remap = null
+var remapping_button = null
+var input_actions = {
+	"player_jump" : "Jump",
+	"player_move_left" : "Move left",
+	"player_move_right" : "Move right",
+	"player_attack" : "Attack",
+	"player_dash" : "Dasssh dash dash!"
+}
 func resume():
 	$AnimationPlayer.play_backwards("Blur")
 
@@ -11,6 +24,8 @@ func pause():
 func _ready():
 	$PanelContainer/VBoxContainer/Volume.value = AudioServer.get_bus_volume_linear(0)*100
 	$"PanelContainer/VBoxContainer/Mute sound".button_pressed = AudioServer.is_bus_mute(0)
+	
+	_create_action_list()
 	
 	for resolution in resolutions:
 		$PanelContainer/VBoxContainer/Resolutions.add_item(str(resolution.x)+"x"+str(resolution.y))
@@ -44,3 +59,51 @@ func _unhandled_input(event: InputEvent) -> void:
 func _on_exit_settings_pressed() -> void:
 		resume()
 		#queue_free()  # Removes this settings menu from the scene
+func _create_action_list():
+	InputMap.load_from_project_settings()
+	for item in action_list.get_children():
+		item.queue_free()
+	for action in input_actions:
+		var button = input_button_scene.instantiate()
+		var action_label = button.find_child("LabelAction")
+		var input_label = button.find_child("LabelInput")
+		
+		action_label.text = input_actions[action]
+		
+		var events = InputMap.action_get_events(action)
+		if events.size() > 0:
+			input_label.text = events[0].as_text().trim_suffix(" (Physical)")
+		else:
+			input_label.text = ""
+			
+		action_list.add_child(button)
+		button.pressed.connect(_on_input_button_pressed.bind(button,action))
+		
+		
+func _on_input_button_pressed(button, action):
+	if !is_remaping:
+		is_remaping = true
+		action_to_remap = action
+		remapping_button = button
+		button.find_child("LabelInput").text = "Press key to bind..."
+				
+func _input(event):
+	if is_remaping:
+		if(
+			event is InputEventKey || 
+			(event is InputEventMouseButton && event.pressed)
+		):
+			InputMap.action_erase_events(action_to_remap)
+			InputMap.action_add_event(action_to_remap, event)
+			_update_action_list(remapping_button, event)
+			
+			is_remaping = false
+			action_to_remap = null
+			remapping_button = null
+			accept_event()
+func _update_action_list(button, event):
+	button.find_child("LabelInput").text = event.as_text().trim_suffix(" (Physical)")			
+
+
+func _on_reset_pressed() -> void:
+	_create_action_list()
